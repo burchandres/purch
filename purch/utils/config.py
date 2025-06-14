95e12428e5f4f6bd91b81892162afcb25d0fc92c
cewbeall <cewbeall@gmail.com>
2025-06-14 18:46:24 -0400

    Adding .env file as a secret in docker

--- purch/utils/config.py
+++ purch/utils/config.py
@@ -11,7 +11,7 @@ from purch.core.finance import PlaidEnvs
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", case_sensitive=True
+        env_file="/run/secrets/env_file", env_file_encoding="utf-8", case_sensitive=True
     )
 
     PRODUCT_NAME: str = Field(default="Purch", description="Name of the finance app")
