f7f1f16a94735aff06d81d8f0f36befa183fc4c9
Andres Burch <acburch22@gmail.com>
2025-06-20 20:28:53 -0400

    add_new_transactions method to budget repo

--- purch/domains/budget/repository.py
+++ purch/domains/budget/repository.py
@@ -1,5 +1,10 @@
-from purch.common.repository import AbstractPostgresRepository
+from sqlmodel import Session
 
+from purch.common.repository import AbstractPostgresRepository
+from purch.domains.models import Transaction
 
 class BudgetRepository(AbstractPostgresRepository):
-    pass
+    def add_new_transactions(self, transactions: list[Transaction]):
+        with Session(self.engine) as session:
+            session.add_all(transactions)
+            session.commit()
