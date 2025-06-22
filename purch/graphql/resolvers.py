f74e699d191e871680f26d7d6b9250967553483c
cewbeall <cewbeall@gmail.com>
2025-06-21 23:59:35 -0400

    Adding more resolvers, using sql queries directly

--- purch/graphql/resolvers.py
+++ purch/graphql/resolvers.py
@@ -1,32 +1,71 @@
 from ariadne import gql, QueryType, ObjectType, load_schema_from_path
-from purch.api.routers.user import get_user_repository
-from purch.domains.finance.repository import FinanceRepository
-from purch.domains.user.repository import UserRepository
+
+from purch.common.config import get_settings
+from purch.domains.models import User, Item, Transaction, Account
+
+from sqlmodel import SQLModel, create_engine, Session, select
+
+
+settings = get_settings()
+engine_url = settings.get_postgres_url()
+engine = create_engine(engine_url, echo=True)
+SQLModel.metadata.create_all(engine)
 
 schema = load_schema_from_path("purch/graphql/schema.graphql")
 
 query = QueryType()
-user_repository = UserRepository()
-finance_repository = FinanceRepository()
+user = ObjectType("User")
+item = ObjectType("Item")
+transaction = ObjectType("Transaction")
+account = ObjectType("Account")
 
 # TODO: test these
+# TODO: figure out how to resolve fields that are not directly on the db models
+
 # USER RESOLUTION
 @query.field("Users")
 def resolve_users(*_):
-    return user_repository.get_all()
-
-user = ObjectType("User")
+    with Session(engine) as session:
+        statement = select(User)
+        results = session.exec(statement)
+        return results.all()
 
 @user.field("item")
-def resolve_items(obj, _):
-    # TODO: add get item by user id to finance repository
-    # finance_repository.get(obj)
-    return
+def resolve_item(obj, _):
+    with Session(engine) as session:
+        statement = select(Item).where(Item.user_id == obj.user_id)
+        results = session.exec(statement)
+        return results.first()
 
 @user.field("transactions")
-def resolve_transactions(obj, _):
-    # TODO: add get transaction by user id to finance repository
-    return
+def resolve_transaction(obj, _):
+    with Session(engine) as session:
+        statement = select(Transaction).where(Transaction.user_id == obj.user_id)
+        results = session.exec(statement)
+        return results.first()
 
 # TRANSACTION RESOLUTION
+@query.field("transactions")
+def resolve_transactions(*, _):  # TODO: Need diff function name? Does decorator distinguish it?
+    with Session(engine) as session:
+        statement = select(Transaction)
+        results = session.exec(statement)
+        return results.all()
 
+@transaction.field("account")
+def resolve_account(obj, _):
+    with Session(engine) as session:
+        statement = select(Account).where(obj.account_id == Account.id)
+        results = session.exec(statement)
+        return results.first()
+
+@transaction.field("user")
+def resolve_user(obj, _):
+    with Session(engine) as session:
+        statement = select(User).where(obj.user_id == User.id)
+        results = session.exec(statement)
+        return results.first()
+
+# ITEM RESOLUTION
+
+# ACCOUNT RESOLUTION
