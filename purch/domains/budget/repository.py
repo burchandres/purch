72043335c8d4c3c6d5150988661cc67ad7ee2813
Andres Burch <acburch22@gmail.com>
2025-06-21 20:14:02 -0400

    followed sqlalchemy documentation for bulk updates, deletes and inserts

--- purch/domains/budget/repository.py
+++ purch/domains/budget/repository.py
@@ -1,8 +1,9 @@
-from sqlmodel import Session, update, delete
+from sqlmodel import Session, update, delete, insert
 
 from purch.common.repository import AbstractPostgresRepository
 from purch.domains.models import Transaction
 from purch.domains.budget.schemas import TransactionUpdate
+
 
 class BudgetRepository(AbstractPostgresRepository):
     def add_new_transactions(self, transactions: list[Transaction]):
@@ -12,17 +13,12 @@ class BudgetRepository(AbstractPostgresRepository):
 
     def update_transactions(self, transactions: list[TransactionUpdate]):
         with Session(self.engine) as session:
-            for transaction_to_update in transactions:
-                statement = (
-                    update(Transaction)
-                    .where(Transaction.id == transaction_to_update.pending_transaction_id)
-                    .values(transaction_to_update.to_dict())
-                )
-                session.exec(statement)
+            statement = update(Transaction)
+            session.exec(statement, [txn.to_dict() for txn in transactions])
             session.commit()
 
     def delete_transactions(self, transaction_ids: list[str]):
         with Session(self.engine) as session:
-            statement = delete(Transaction).where(Transaction.id in transaction_ids)
-            session.exec(statement)
+            statement = delete(Transaction)
+            session.exec(statement, [{"id": txn_id for txn_id in transaction_ids}])
             session.commit()
